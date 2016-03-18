b8ba5d94e683ef11e9252da517e025d29090fd8c
Sung Kang <sungjk@msn.com>
2016-03-18 14:01:59 -0700

    corrected db address

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
 var path = require('path');
-mongoose.connect('mongodb://user1:dkssud1@ds057954.mongolab.com:57954/heroku_754s2lbl');
+mongoose.connect('mongodb://sung:dkssud1@ds015849.mlab.com:15849/heroku_c9w6j14s');
 var models_path = path.join(__dirname, './../models');
 fs.readdirSync(models_path).forEach(function(file){
 	if(file.indexOf('.js') >= 0){
